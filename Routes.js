64519a8755e96bb7e45e4042616c53f24c2a4b13
VladCyberconmnunist <vladcyberconmnunist@gmail.com>
2025-02-25 18:19:41 +0900

    Update Routes.js

--- Routes.js
+++ Routes.js
@@ -14,20 +14,7 @@ const menuContent=document.querySelectorAll("li")
    console.log(routes[i].slice(1))
     // window.location.assign('/HTMLContent.html')
 });
-// menuContent.forEach((e) => {
-//     // e.setAttribute('onclick',SetRoute(routes[i]))
-//     // if (curRoute==e.getAttribute("[onclick]")){}
-//    let fullRoute=e.getAttribute('onclick')
-//     console.log(fullRoute)
-//     let pureRoute=fullRoute.substring(fullRoute.lastIndexOf('/')+1)
-//     console.log(pureRoute)
-// });
-    // let locactive=document.location.href
-    // console.log(locactive)
-   
-// function htmlturn(){
-//     window.location.assign('/HTMLContent.html')
-// }
+
 function SetRoute(routePath){
    return "window.location.assign("+"\""+routePath+"\""+")"
 }
