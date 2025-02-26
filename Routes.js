e0dadbb686eb3a6751a7b249d9ae21ade60e114c
Vlad Cyberconmnunist <vladcyberconmnunist@gmail.com>
2025-02-26 11:01:05 +0900

    Routes remove console.log

--- Routes.js
+++ Routes.js
@@ -1,18 +1,13 @@
 let url = window.location.pathname;
 let curRoute = url.substring(url.lastIndexOf('/')+1);
-console.log(curRoute)
 
 const menuContent=document.querySelectorAll("li")
-    // console.log(menuContent)
     const routes = ['/HTMLContent.html','/CSSContent.html','/HTMLNCSSContent.html','/JSContent.html','/SimpleProjects.html']
-    // console.log(routes)
     menuContent.forEach((e,i) => {
     e.setAttribute('onclick',SetRoute(routes[i]))
     if (curRoute==routes[i].slice(1)){
         e.classList.add("active")
     }
-   console.log(routes[i].slice(1))
-    // window.location.assign('/HTMLContent.html')
 });
 
 function SetRoute(routePath){
